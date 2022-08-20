4c3dd00e8b2828183d5eab910259fd809bc2c0e3
Regber <regber.888@gmail.com>
2022-08-20 17:24:24 +0300

    Первый коммит.

---
+++ WordCountApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WordCountApp
+{
+    class Program
+    {
+        static void Main(string[] filePaths)
+        {
+            Console.WriteLine("Start!");
+
+            foreach (var path in filePaths)
+            {
+                Console.WriteLine(path);
+            }
+
+            Console.WriteLine("Completed!");
+
+            Console.ReadLine();
+        }
+    }
+}
